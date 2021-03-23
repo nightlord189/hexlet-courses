53cdcb27332db9c26b62f979938005104c775296
Alexey Buravov <night.lord.189@gmail.com>
2021-03-24 04:22:19 +0600

    js-react start

---
+++ js-react/01-component/Index.jsx
@@ -0,0 +1,10 @@
+// @ts-check
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Card from './Card.jsx';
+
+// BEGIN (write your solution here)
+const mountNode = document.getElementById('container');
+ReactDOM.render(<Card />, mountNode);
+// END
